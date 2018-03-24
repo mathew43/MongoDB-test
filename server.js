6c9a981eb97610bb71b563fbdfeb5040f121903d
Mateusz Łukowski <mateusz.lukowski@wp.pl>
2018-03-24 22:24:44 +0100

    add to mlab

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/nodeappdatabase', {
+mongoose.connect('mongodb://database:database@ds223509.mlab.com:23509/mongo', {
     useMongoClient: true
 });
 
